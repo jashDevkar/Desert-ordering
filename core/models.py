77cc28c5e6207794ae43f12e7ecd8f8eda010911
jashDevkar <devkarjash1054@gmail.com>
2024-03-26 19:07:31 +0530

    meta data

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Student(models.Model):
+    name = models.CharField(max_length=20)
+    major = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+    
